ecaabab0e6b2b188411b48f5c36f71a2ac7d7c2f
Suyash Savji <savjisuyash@gmail.com>
2020-11-24 11:37:34 +0530

    Commit before switching to master

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import './Person.css';
+import classes from './Person.css';
 
 
 
 
 const person = (props) => {
 
+    const rnd = Math.random();
+
+    if(rnd > 0.7){
+        throw new Error('Something Went Wrong!');
+    }
+
     return (
-        <div className="Person">
+        <div className={classes.Person}>
         <p onClick={props.click}>My name is {props.name} and I am {props.age} years old!</p>
         <p>{props.children}</p>
         <input type="text" onChange={props.changed} value={props.name} />
